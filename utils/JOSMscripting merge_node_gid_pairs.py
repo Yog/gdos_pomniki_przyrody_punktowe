b896d7ff15b0d02597f3dc13cebaa9e966dad837
Piotr Rorot <piotr.rorot@gmail.com>
2026-02-14 20:11:53 +0100

    merging nodes with same gid but always keeping the lowest (oldest) node

--- utils/JOSMscripting merge_node_gid_pairs.py
+++ utils/JOSMscripting merge_node_gid_pairs.py
@@ -3,7 +3,6 @@ from org.openstreetmap.josm.data.osm import Node, TagMap
 from org.openstreetmap.josm.data import UndoRedoHandler
 from org.openstreetmap.josm.command import ChangeCommand, DeleteCommand, SequenceCommand
 
-# Get current edit layer
 layer = MainApplication.getLayerManager().getEditLayer()
 
 if layer and layer.data:
@@ -14,15 +13,12 @@ if layer and layer.data:
         print("No nodes selected. Please select nodes first.")
     else:
 
-        # Group nodes by ref:gid value
+        # Group nodes by ref:gid
         groups = {}
-
         for node in selected_nodes:
             gid = node.get("ref:gid")
             if gid:
-                if gid not in groups:
-                    groups[gid] = []
-                groups[gid].append(node)
+                groups.setdefault(gid, []).append(node)
 
         commands = []
         merged_count = 0
@@ -30,32 +26,33 @@ if layer and layer.data:
         for gid, nodes in groups.items():
             if len(nodes) > 1:
 
-                # ?? Prefer OSM server node (isNew() == False)
-                nodes_sorted = sorted(nodes, key=lambda n: n.isNew())
-                target_node = nodes_sorted[0]
+                # Separate server and new nodes
+                server_nodes = [n for n in nodes if not n.isNew()]
 
+                if server_nodes:
+                    # Keep server node with lowest positive ID
+                    target_node = min(server_nodes, key=lambda n: n.getId())
+                else:
+                    # All nodes are new > keep lowest ID (closest to zero)
+                    target_node = min(nodes, key=lambda n: n.getId())
+
+                # Merge tags
                 tags_to_merge = {}
-
-                # Merge tags from all nodes
                 for node in nodes:
                     tags = node.getInterestingTags()
                     for k, v in tags.items():
-                        if k not in tags_to_merge:
-                            tags_to_merge[k] = set()
-                        tags_to_merge[k].add(unicode(v))
+                        tags_to_merge.setdefault(k, set()).add(unicode(v))
 
-                # Create merged tag map
                 new_tags = TagMap()
                 for k, values in tags_to_merge.items():
                     new_tags.put(k, u";".join(sorted(values)))
 
-                # Clone and update target node
                 updated_node = Node(target_node)
                 updated_node.setKeys(new_tags)
 
                 commands.append(ChangeCommand(target_node, updated_node))
 
-                # Delete all other nodes except the chosen target
+                # Delete all others
                 for node in nodes:
                     if node != target_node:
                         commands.append(DeleteCommand(node))
@@ -64,7 +61,12 @@ if layer and layer.data:
 
         if commands:
             undo_handler = UndoRedoHandler.getInstance()
-            undo_handler.add(SequenceCommand(u"Merge Nodes by ref:gid (Prefer OSM)", commands))
+            undo_handler.add(
+                SequenceCommand(
+                    u"Merge Nodes by ref:gid (Prefer OSM, Lowest ID)",
+                    commands
+                )
+            )
             print(u"Success: Merged {} ref:gid groups.".format(merged_count))
         else:
             print("No duplicate ref:gid values found among selected nodes.")
